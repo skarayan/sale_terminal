83a231a08f4832e01c366945df5810863627f521
Sarkis Karayan <skarayan@gmail.com>
2011-08-08 09:14:23 -0400

    Removed commented code

--- models/product.rb
+++ models/product.rb
@@ -5,21 +5,6 @@ class Product < ActiveRecord::Base
   validates_presence_of :product_code
   validates_uniqueness_of :product_code
 
-  #def self.all
-  #  ObjectSpace.each_object(Product)
-  #end
-
-  #def self.find_by_product_code(product_code)
-  #  self.all.each { |product| return product if product.product_code == product_code }
-  #  nil
-  #end
-
-  #def initialize(product_code)
-  #  raise ProductAlreadyCreated if Product.find_by_product_code(product_code)
-  #  @product_code = product_code
-  #  @prices = {}
-  #end
-
   def hash_prices
     prices.inject({}) do |hash,price|
       hash[price.volume] = price.price
